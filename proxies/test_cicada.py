48af39668d795bc78df13d7ca4bfe1e218761574
arcadiainmayhem <doodleypants@gmail.com>
2026-09-22 21:31:36 +0800

    added BUGFRAME print

--- proxies/test_cicada.py
+++ proxies/test_cicada.py
@@ -4,6 +4,8 @@ from swamp.broodswarm import Broodswarm
 from swamp.cicadaConstants import BROOD_IDS
 from hardware.hardwareConstants import MOTHER_PORT,MOTHER_BAUD
 import time
+from swamp.cicadaConstants import BUG_FRAME
+
 
 buglink = BugLinkManager( MOTHER_PORT, MOTHER_BAUD) 
 
@@ -11,7 +13,7 @@ buglink = BugLinkManager( MOTHER_PORT, MOTHER_BAUD)
 #open
 buglink.open()
 print("[SWAMPBED] MOTHER ESP LINKED TO MOTHERPI VIA SERIAL")
-
+print(list(BUG_FRAME))
 
 swarm = Broodswarm(BROOD_IDS)
 
@@ -21,7 +23,7 @@ try:
 
     while (buglink.is_connected()):
         time.sleep(0.1)
-
+        print("[TEST] POLLING ")
         frames = buglink.poll()
 
         #check against empty
